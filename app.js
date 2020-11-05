abd9b383b652e8f2c63794f2b81386bee78757ac
Oleksiuk Bohdan <lordapos@gmail.com>
2020-11-05 19:13:50 +0200

    add scss build

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express')
 const path = require('path')
 const nunjucks = require('nunjucks')
 const app = express()
-const homeRoutes = require('./routes/home');
-const coursesRoutes = require('./routes/courses');
-const addRoutes = require('./routes/add');
+const homeRoutes = require('./routes/home')
+const coursesRoutes = require('./routes/courses')
+const addRoutes = require('./routes/add')
+const sassMiddleware = require('node-sass-middleware')
+
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'assets/scss'),
+    dest: path.join(__dirname, 'public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/css'
+}))
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', homeRoutes)
---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
