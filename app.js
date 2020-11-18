72838f6c8728c0c7bee422eadb3f0b30eb8874df
Oleksiuk Bohdan <lordapos@gmail.com>
2020-11-18 13:38:59 +0200

    add card models

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app = express()
 const homeRoutes = require('./routes/home')
 const coursesRoutes = require('./routes/courses')
 const addRoutes = require('./routes/add')
+const cardRoutes = require('./routes/card')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
+app.use('/card', cardRoutes)
 const env = nunjucks.configure(['views/'], {
     autoescape: true,
     express: app
