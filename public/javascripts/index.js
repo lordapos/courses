9ce59c9c34a728eacf0017da152a0f5defc0a1fa
Oleksiuk Bohdan <lordapos@gmail.com>
2020-11-18 15:10:10 +0200

    change dynamic view card

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach(function (node) {
-    node.textContent = new Intl.NumberFormat('en-US', {
+const toCurrency = price => {
+    return new Intl.NumberFormat('en-US', {
         currency: 'USD',
         style: 'currency'
-    }).format(node.textContent)
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(function (node) {
+    node.textContent = toCurrency(node.textContent)
 })
 
 const $card = document.querySelector('#card')
@@ -15,7 +19,22 @@ if ($card) {
                 method: 'delete'
             }).then(res => res.json())
                 .then(card => {
-                    console.log(card)
+                    if (card.courses.length) {
+                        const html = card.courses.map(c => {
+                            return `
+                            <tr>
+                                <td>${ c.title }</td>
+                                <td>${ c.count }</td>
+                                <td>
+                                    <button class="delete js-remove" data-id="${ c.id }">Delete</button>
+                                </td>
+                            </tr>
+                            `}).join('')
+                        $card.querySelector('tbody').innerHTML = html
+                        $card.querySelector('.price').textContent = toCurrency(card.price)
+                    } else {
+                        $card.querySelector('.basket__content').innerHTML = '<p class="basket__empty">Empty</p>'
+                    }
                 })
         }
     })
