5811e7e1b1769a0e520d069fba98d6c94a1e8749
Oleksiuk Bohdan <lordapos@gmail.com>
2020-12-30 20:15:58 +0200

    auth logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const router = Router()
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
     res.render('auth/login.njk', {
@@ -10,21 +11,55 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    const user = await User.findById('5fe492bc4e88e70b247e7a22')
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err => {
-        if (err) {
-            throw err
+    try {
+        const {email, password} = req.body
+        const candidate = await User.findOne({ email })
+        if (candidate) {
+            const areSame = await bcrypt.compare(password, candidate.password)
+            if (areSame) {
+                const user = candidate
+                req.session.user = user
+                req.session.isAuthenticated = true
+                req.session.save(err => {
+                    if (err) {
+                        throw err
+                    }
+                    res.redirect('/')
+                })
+            } else {
+                res.redirect('/auth/login#login')
+            }
+        } else {
+            res.redirect('/auth/login#login')
         }
-        res.redirect('/')
-    })
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 router.get('/logout', async (req, res) => {
     req.session.destroy(() => {
         res.redirect('/auth/login')
     })
+})
+
+router.post('/register', async (req, res) => {
+    try {
+        const {email, password, repeat, name} = req.body
+        const candidate = await User.findOne({ email })
+        if (candidate) {
+            res.redirect('/auth/login#register')
+        } else {
+            const hashPassword = await bcrypt.hash(password, 10)
+            const user = new User({
+                email, name, password: hashPassword, cart: {items: []}
+            })
+            await user.save()
+            res.redirect('/auth/login#login')
+        }
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 module.exports = router
