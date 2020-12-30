63b5d00fa6379bc13818833c28a8169d2c518de1
Oleksiuk Bohdan <lordapos@gmail.com>
2020-12-30 19:08:21 +0200

    sessions logic

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const nunjucks = require('nunjucks')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const app = express()
 const homeRoutes = require('./routes/home')
 const coursesRoutes = require('./routes/courses')
@@ -10,19 +12,23 @@ const ordersRoutes = require('./routes/orders')
 const cardRoutes = require('./routes/card')
 const authRoutes = require('./routes/auth')
 const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
 
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('5fe492bc4e88e70b247e7a22')
-        req.user = user
-        next()
-    } catch (e) {
-        console.log(e)
-    }
-})
-
+const MONGODB_URI = 'mongodb+srv://cyber:QU4EXHZEwVxFp6j4@courses.op53b.mongodb.net/shop'
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
+
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MONGODB_URI,
+})
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}))
+app.use(varMiddleware)
 app.use('/', homeRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/add', addRoutes)
@@ -32,27 +38,18 @@ app.use('/auth', authRoutes)
 const env = nunjucks.configure(['views/'], {
     autoescape: true,
     express: app
-});
+})
+
 
 const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
-        const url = 'mongodb+srv://cyber:QU4EXHZEwVxFp6j4@courses.op53b.mongodb.net/shop'
-        await  mongoose.connect(url, {
+        await  mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
         })
-        const candidate = await User.findOne()
-        if (!candidate) {
-            const user = new User({
-                email: 'test@gmail.com',
-                name: 'Rob',
-                cart: {items: []}
-            })
-            await user.save()
-        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,29 @@
 const {Router} = require('express')
 const router = Router()
+const User = require('../models/user')
 
 router.get('/login', (req, res) => {
     res.render('auth/login.njk', {
         title: 'Authentication',
         isLogin: true,
+    })
+})
+
+router.post('/login', async (req, res) => {
+    const user = await User.findById('5fe492bc4e88e70b247e7a22')
+    req.session.user = user
+    req.session.isAuthenticated = true
+    req.session.save(err => {
+        if (err) {
+            throw err
+        }
+        res.redirect('/')
+    })
+})
+
+router.get('/logout', async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/auth/login')
     })
 })
 
